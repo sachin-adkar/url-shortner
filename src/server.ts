f9c373999e32bef0d3c8e90d04d96f4f74872054
Sachin <sachin.adkarr@gmail.com>
2021-09-27 18:50:25 +0530

    Added ts-watch

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
-import express, {Request, Response}  from 'express';
+import express from 'express';
 import { config } from './configs/configurations';
 import connect from './db/connect';
 import './api/auth/getAuthUri';
 import routes from './api';
-import path from 'path';
 import cors from 'cors';
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
-
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 // Check if configs are loaded
 if (!config.dbUri || !config.rootUrl)
